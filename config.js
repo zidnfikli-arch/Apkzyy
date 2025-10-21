86fc0993779575d4ebfbc2f0a2a61db32e41461a
zidnfikli-arch <zidnfikli@gmail.com>
2025-10-21 18:15:29 +0700

    Create config.js with bot configuration
    Add configuration settings for bot tokens and server details.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8315139539:AAH97iiMuqYApbI2z_5QQ2jX0p38oqmfKoM",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7860329124", // Ubah Jadi Id Mu !!!
+  port: "25", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "http://zyysukajanda.pterodactyl-panel.web.id" // Ubah Jadi Ip Vps Mu !!!
+};
